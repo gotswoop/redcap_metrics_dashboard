b6aa79cb9e949099945b5f2cb2e3650d494636e5
Swaroop Samek <swaroop.samek@gmail.com>
2026-04-13 18:53:57 -0700

    Checking in config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+	'connect_file' => '/path/to/redcap/redcap_connect.php',
+
+	'app_title' => 'Research REDCap Metrics Dashboard',
+
+	'instance_name' => 'Research REDCap',
+
+	'subtitle' => 'Key metrics across the Research REDCap instance.'
+];
